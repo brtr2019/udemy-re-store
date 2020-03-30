60904671dbe493c3fbbbc4782d2989542d291c71
Laert <laert1983@mail.ru>
2020-03-30 15:35:37 -0700

    Store enhancer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,37 @@
-import {createStore} from "redux";
+import {createStore,compose} from "redux";
 import reducer from './reducers/index';
 
-const store = createStore(reducer);
+const stringEnhancer  = (createStore)=>(...args)=>{
+    const store = createStore(...args);
+    const originalDipatch = store.dispatch;
+    store.dispatch = (action)=>{
+        if(typeof action==="string"){
+            return originalDipatch({
+                type:action
+            })
+        }
+        else {
+            originalDipatch(action);
+        }
+    }
+    return store;
+}
+
+const logEnhancer  = (createStore)=>(...args)=>{
+    const store = createStore(...args);
+    const originalDipatch = store.dispatch;
+    store.dispatch = (action)=>{
+            console.log(action.type);
+            originalDipatch(action);
+        }
+    return store;
+
+}
+
+
+const store = createStore(reducer,compose(stringEnhancer,logEnhancer));
+
+
+store.dispatch('HELLO_WORLD');
 
 export default store;
